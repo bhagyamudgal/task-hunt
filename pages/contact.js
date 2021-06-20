4a3fbf26b12b3cdaae5b81c67be3df66202235e8
Bhagya Mudgal <bhagyamudgal@gmail.com>
2021-06-20 13:56:49 +0530

    Created web pages paths for website

---
+++ pages/contact.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Navigation from "../components/Navigation";
+
+function ContactPage() {
+  return (
+    <>
+      <Navigation page="contact"></Navigation>
+      <h1>The Contacts Page</h1>
+    </>
+  );
+}
+
+export default ContactPage;
---
+++ pages/features.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Navigation from "../components/Navigation";
+
+function FeaturesPage() {
+  return (
+    <>
+      <Navigation page="features"></Navigation>
+      <h1>The Features Page</h1>
+    </>
+  );
+}
+
+export default FeaturesPage;
