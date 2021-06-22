cfb3465fd3bc8b03cbd736a2a9df9e9e62d94727
Bhagya Mudgal <bhagyamudgal@gmail.com>
2021-06-22 09:21:50 +0530

    added heading div and reconfigure buttons

--- components/Button.js
+++ components/Button.js
@@ -1,14 +1,18 @@
 import React from "react";
 import Link from "next/link";
+import { useState } from "react";
 import styles from "../components/Button.module.css";
 
 function Button(props) {
+
+  const [boxShadow, setBoxShadow] = useState(props.boxshadow);
+  const [boxType, setBoxType] = useState(props.boxtype);
   return (
     <Link href={props.target} passHref>
       <button
         type="button"
-        className={styles.button}
-        style={{ width: props.width }}
+        className={`${boxType ? styles.button : styles.button_navigation} ${boxShadow ? styles.box_shadow : " "}`}
+        style={{ width: props.width}}
       >{props.text}</button>
     </Link>
   );
--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,21 @@
 import Navigation from "../components/Navigation";
+import Button from "../components/Button";
 import styles from "../styles/home.module.css";
 
 function HomePage(params) {
   return (
     <div className={styles.body_home}>
       <Navigation page="home"></Navigation>
+      <div className={styles.heading_div}>
+        <h1>
+          A well designed solution for tracking Assignments.
+        </h1>
+      </div>
+
+      <div className={styles.button_div}>
+      <Button target="/signup" width="240px" text="Register Now" boxshadow="true" boxtype="true">Signup</Button>
+      <Button target="/login" text="Login" width="200px" boxshadow="true" boxtype="true">Login</Button>
+      </div>
     </div>
   );
 }
