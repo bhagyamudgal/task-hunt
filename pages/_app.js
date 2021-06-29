34ca9d8cdd9438a0e2cff5241b4dc84af362a663
Bhagya Mudgal <bhagyamudgal@gmail.com>
2021-06-29 07:28:32 +0530

    added favicons and meta tag

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,5 @@
 import "../styles/globals.css";
+import Head from "next/head";
 import Layout from "../layout/Layout";
 import Ellipse from "../components/Ellipse";
 import { useRouter } from "next/router";
@@ -7,8 +8,31 @@ function MyApp({ Component, pageProps }) {
   const router = useRouter();
   return (
     <Layout>
+      <Head>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/site.webmanifest" />
+      </Head>
       {router.pathname === "/features" && <Ellipse></Ellipse>}
       {router.pathname === "/contact" && <Ellipse></Ellipse>}
+      {router.pathname === "/signup" && <Ellipse></Ellipse>}
+      {router.pathname === "/login" && <Ellipse></Ellipse>}
       <Component {...pageProps} />
     </Layout>
   );
