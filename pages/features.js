1d39b866d3354c54b2ed87d1efbb1efc4db001a5
Bhagya Mudgal <bhagyamudgal@gmail.com>
2021-06-29 07:30:15 +0530

    modified features

--- pages/features.js
+++ pages/features.js
@@ -1,27 +1,24 @@
 import React from "react";
-import Image from "next/image";
 import styles from "../styles/features.module.css";
 
 function FeaturesPage() {
   return (
-    <>
-      <div className={styles.body_features}>
-        <div className={styles.heading_div}>
-          <h1>Features</h1>
-        </div>
-        <div className={styles.points_div}>
-          <ul>
-            <li>
-              All in one platform for teachers and students to work on
-              assignments.
-            </li>
-            <li>Share assignments in few minutes.</li>
-            <li>Notification for every assignment shared.</li>
-            <li>Grade assignments and generate report.</li>
-          </ul>
-        </div>
+    <div className={styles.body_features}>
+      <div className={styles.heading_div}>
+        <h1>Features</h1>
       </div>
-    </>
+      <div className={styles.points_div}>
+        <ul>
+          <li>
+            All in one platform for teachers and students to work on
+            assignments.
+          </li>
+          <li>Share assignments in few minutes.</li>
+          <li>Notification for every assignment shared.</li>
+          <li>Grade assignments and generate report.</li>
+        </ul>
+      </div>
+    </div>
   );
 }
 
