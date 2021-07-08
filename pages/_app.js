3778b1e88d830555184c44f42641b76aeae61700
Bhagya Mudgal <bhagyamudgal@gmail.com>
2021-07-08 22:28:42 +0530

    added next auth provider wrapper

--- pages/_app.js
+++ pages/_app.js
@@ -3,6 +3,7 @@ import Head from "next/head";
 import Layout from "../layout/Layout";
 import Ellipse from "../components/Ellipse";
 import Loading from "../components/Loading";
+import { Provider } from "next-auth/client";
 import { useState, useEffect } from "react";
 import { useRouter } from "next/router";
 
@@ -40,38 +41,40 @@ function MyApp({ Component, pageProps }) {
 
   return (
     <>
-      <Loading loading={loading} />
-      <Layout loading={loading} onload={loadingHandler}>
-        <Head>
-          <meta
-            name="viewport"
-            content="initial-scale=1.0, width=device-width"
-          />
-          <link
-            rel="apple-touch-icon"
-            sizes="180x180"
-            href="/apple-touch-icon.png"
-          />
-          <link
-            rel="icon"
-            type="image/png"
-            sizes="32x32"
-            href="/favicon-32x32.png"
-          />
-          <link
-            rel="icon"
-            type="image/png"
-            sizes="16x16"
-            href="/favicon-16x16.png"
-          />
-          <link rel="manifest" href="/site.webmanifest" />
-        </Head>
-        {router.pathname === "/features" && <Ellipse></Ellipse>}
-        {router.pathname === "/contact" && <Ellipse></Ellipse>}
-        {router.pathname === "/signup" && <Ellipse></Ellipse>}
-        {router.pathname === "/login" && <Ellipse></Ellipse>}
-        <Component {...pageProps} />
-      </Layout>
+      <Provider session={pageProps.session}>
+        <Loading loading={loading} />
+        <Layout loading={loading} onload={loadingHandler}>
+          <Head>
+            <meta
+              name="viewport"
+              content="initial-scale=1.0, width=device-width"
+            />
+            <link
+              rel="apple-touch-icon"
+              sizes="180x180"
+              href="/apple-touch-icon.png"
+            />
+            <link
+              rel="icon"
+              type="image/png"
+              sizes="32x32"
+              href="/favicon-32x32.png"
+            />
+            <link
+              rel="icon"
+              type="image/png"
+              sizes="16x16"
+              href="/favicon-16x16.png"
+            />
+            <link rel="manifest" href="/site.webmanifest" />
+          </Head>
+          {router.pathname === "/features" && <Ellipse></Ellipse>}
+          {router.pathname === "/contact" && <Ellipse></Ellipse>}
+          {router.pathname === "/signup" && <Ellipse></Ellipse>}
+          {router.pathname === "/login" && <Ellipse></Ellipse>}
+          <Component {...pageProps} />
+        </Layout>
+      </Provider>
     </>
   );
 }
