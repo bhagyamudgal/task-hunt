e44a324b1d8dbb3164c295910a3d433bcae46ddd
Bhagya Mudgal <bhagyamudgal@gmail.com>
2021-07-29 17:39:46 +0530

    changed display button from div to span

--- dashboard_display_components/DisplayButton.js
+++ dashboard_display_components/DisplayButton.js
@@ -3,9 +3,9 @@ import styles from "./DisplayButton.module.css";
 
 function DisplayButton(props) {
   return (
-    <div className={styles.display_button} onClick={props.onClick}>
+    <span className={styles.display_button} onClick={props.onClick}>
       <h2>{props.text}</h2>
-    </div>
+    </span>
   );
 }
 
