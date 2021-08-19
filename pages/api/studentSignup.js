15983cb923cb9d3339880928d906bdd6405f0462
Bhagya Mudgal <bhagyamudgal@gmail.com>
2021-08-19 23:44:09 +0530

    added signup email functionality

--- pages/api/studentSignup.js
+++ pages/api/studentSignup.js
@@ -1,4 +1,5 @@
 import { connectToDatabase } from "../../lib/db";
+import sgMail from "@sendgrid/mail";
 import { hash } from "bcryptjs";
 const validator = require("validator");
 const generator = require("generate-password");
@@ -45,8 +46,8 @@ async function handler(req, res) {
     length: 10,
     numbers: true,
   });
-  console.log(username);
-  console.log(password);
+  // console.log(username);
+  // console.log(password);
   const hashPassword = await hash(password, 12);
 
   const result = await db.collection("students").insertOne({
@@ -65,6 +66,20 @@ async function handler(req, res) {
   });
   if (result) {
     res.status(201).json({ message: "Student registered successfully" });
+    sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+    const msg = {
+      to: email,
+      from: "taskhuntweb@gmail.com",
+      subject: `Student Registration Details - ${name} | Task Hunt`,
+      text: `Thanks for signing up as a student on Task Hunt, please find your Username and Password below: Username: ${username}, Password: ${password}`,
+      html: `<h3>Thanks for signing up as a student on Task Hunt, please find your Username and Password below:</h3><br><br><h4>Username: ${username}</h4><br><br><h4>Password: ${password}</h4><br><br><h5>This is one time login password after login you will have to change your password.</h5>`,
+    };
+    try {
+      await sgMail.send(msg);
+      console.log(`Email has been sent`);
+    } catch (error) {
+      console.log(error);
+    }
   }
   client.close();
 }
--- pages/api/teacherSignup.js
+++ pages/api/teacherSignup.js
@@ -1,4 +1,5 @@
 import { connectToDatabase } from "../../lib/db";
+import sgMail from "@sendgrid/mail";
 import { hash } from "bcryptjs";
 const validator = require("validator");
 const generator = require("generate-password");
@@ -34,8 +35,8 @@ async function handler(req, res) {
     length: 10,
     numbers: true,
   });
-  console.log(username);
-  console.log(password);
+  // console.log(username);
+  // console.log(password);
   const hashPassword = await hash(password, 12);
 
   const result = await db.collection("teachers").insertOne({
@@ -52,7 +53,22 @@ async function handler(req, res) {
   });
   if (result) {
     res.status(201).json({ message: "Teacher registered successfully" });
+    sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+    const msg = {
+      to: email,
+      from: "taskhuntweb@gmail.com",
+      subject: `Teacher Registration Details - ${name} | Task Hunt`,
+      text: `Thanks for signing up as a teacher on Task Hunt, please find your Username and Password below: Username: ${username}, Password: ${password}`,
+      html: `<h3>Thanks for signing up as a teacher on Task Hunt, please find your Username and Password below:</h3><br><br><h4>Username: ${username}</h4><br><br><h4>Password: ${password}</h4><br><br><h5>This is one time login password after login you will have to change your password.</h5>`,
+    };
+    try {
+      await sgMail.send(msg);
+      console.log(`Email has been sent`);
+    } catch (error) {
+      console.log(error);
+    }
   }
+
   client.close();
 }
 
